25b901b6d6a712e7071b6b9e759f089dd1107ea4
inPhoenix <spiderk@gmail.com>
2019-02-23 00:33:05 +0000

    :rocket: Adding infinite scroll feature

---
+++ client/src/features/InlineLoader.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const InlineLoader = () => {
+  return (
+    <div className="spinner-container">
+      <div className="lds-facebook">
+        <div />
+        <div />
+        <div />
+      </div>
+    </div>
+  )
+}
+
+export default InlineLoader
